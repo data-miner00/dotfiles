c4d4199fab788c7ae8f4f0aa2e09011f4c79ad86
Shaun Chong <pkay_@live.com>
2024-07-27 17:26:07 +0800

    Add keymap descriptions

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -3,46 +3,46 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Vimrc shortcut
-vim.keymap.set('', '<leader>E', ':e $MYVIMRC<cr>')
-vim.keymap.set('', '<leader>S', ':source %<cr>')
+vim.keymap.set('', '<leader>E', ':e $MYVIMRC<cr>', { desc = "[P]Open up the config file for editing" })
+vim.keymap.set('', '<leader>S', ':source %<cr>', { desc = "[P]Reload the edited config file in current session" })
 
 -- Exit
-vim.keymap.set('i', 'jj', '<esc>')
+vim.keymap.set('i', 'jj', '<esc>', { desc = "[P]Exit insert mode" })
 
 -- Search
-vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
-vim.keymap.set('n', 'n', 'nzz')
-vim.keymap.set('n', 'N', 'Nzz')
+vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>', { desc = "[P]Unhighlight the search results" })
+vim.keymap.set('n', 'n', 'nzz', { desc = "[P]Go to the next search result and center it" })
+vim.keymap.set('n', 'N', 'Nzz', { desc = "[P]Go to the previous search result and center it" })
 
 -- Half page move
-vim.keymap.set('n', '<C-d>', '<C-d>zz')
-vim.keymap.set('n', '<C-u>', '<C-u>zz')
+vim.keymap.set('n', '<C-d>', '<C-d>zz', { desc = "[P]Move down half a page and center it" })
+vim.keymap.set('n', '<C-u>', '<C-u>zz', { desc = "[P]Move up half a page and center it" })
 
 -- Clipboard I/O
-vim.keymap.set({ 'n', 'x', 'v' }, '<leader>y', '"+y')
-vim.keymap.set('n', '<leader>Y', '"+Y')
-vim.keymap.set({ 'n', 'x' }, '<leader>p', '"+p')
+vim.keymap.set({ 'n', 'x', 'v' }, '<leader>y', '"+y', { desc = "[P]Copy the text to clipboard" })
+vim.keymap.set('n', '<leader>Y', '"+Y', { desc = "[P]Copy the text to the end of line to clipboard" })
+vim.keymap.set({ 'n', 'x' }, '<leader>p', '"+p', { desc = "[P]Paste the content from clipboard" })
 
 -- Discard contents in 'x' operation
-vim.keymap.set({ 'n', 'x' }, 'x', '"_x')
+vim.keymap.set({ 'n', 'x' }, 'x', '"_x', { desc = "[P]Discard single character into void register" })
 
 -- Discard contents in 'p' operation
-vim.keymap.set('x', '<leader>p', '"_dP')
+vim.keymap.set('x', '<leader>p', '"_dP', { desc = "[P]Discard replaced content into void register" })
 
 -- Discard contents in 'd' operation
-vim.keymap.set({ 'n', 'v' }, '<leader>d', '"_d')
+vim.keymap.set({ 'n', 'v' }, '<leader>d', '"_d', { desc = "[P]Discard deleted(cut) content into void register" })
 
 -- Select all text in current buffer
-vim.keymap.set('n', '<leader>a', ':keepjumps normal! ggVG<cr>')
+vim.keymap.set('n', '<leader>a', ':keepjumps normal! ggVG<cr>', { desc = "[P]Select all text in the buffer" })
 
 -- Window navigation
-vim.keymap.set('n', '<c-h>', '<c-w>h')
-vim.keymap.set('n', '<c-j>', '<c-w>j')
-vim.keymap.set('n', '<c-k>', '<c-w>k')
-vim.keymap.set('n', '<c-l>', '<c-w>l')
+vim.keymap.set('n', '<c-h>', '<c-w>h', { desc = "[P]Focus window to the left" })
+vim.keymap.set('n', '<c-j>', '<c-w>j', { desc = "[P]Focus window to the bottom" })
+vim.keymap.set('n', '<c-k>', '<c-w>k', { desc = "[P]Focus window to the top" })
+vim.keymap.set('n', '<c-l>', '<c-w>l', { desc = "[P]Focus window to the right" })
 
 -- Select whole paragraph
-vim.keymap.set('n', '<leader>v', 'vip')
+vim.keymap.set('n', '<leader>v', 'vip', { desc = "[P]Select the whole paragraph" })
 
 -- Tabs
 vim.keymap.set('', '<leader>tt', ':tabnew<cr>')
@@ -58,20 +58,26 @@ vim.keymap.set('n', '<s-h>', 'gT')
 vim.keymap.set('n', '<s-l>', 'gt')
 
 -- Block move
-vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
-vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv", { desc = "[P]Move the selected block bottom" })
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv", { desc = "[P]Move the selected block top" })
 
 -- Better append
-vim.keymap.set('n', 'J', 'mzJ`z')
+vim.keymap.set('n', 'J', 'mzJ`z', { desc = "[P]Better append of next line" })
 
 -- Format
 vim.keymap.set('n', '<leader>f', function()
   vim.lsp.buf.format()
-end)
+end, { desc = "[P]Format the current buffer with available formatters" })
 
 -- Replace
-vim.keymap.set("n", "<leader>r", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
-vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+vim.keymap.set("n", "<leader>r",
+  [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],
+  { desc = "[P]Replace the word under cursor globally" })
+
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", {
+  desc = "[P]Mark the current file as executable",
+  silent = true
+})
 
 -- Random
 vim.keymap.set("n", "<leader>fml", "<cmd>CellularAutomaton make_it_rain<cr>")
@@ -81,6 +87,6 @@ vim.keymap.set('n', '<c-s-h>', '20zh')
 vim.keymap.set('n', '<c-s-l>', '20zl')
 
 -- Terminal operation
-vim.keymap.set('n', '<c-o><c-t>', ':terminal<cr>')
+vim.keymap.set('n', '<c-o><c-t>', ':terminal<cr>', { desc = "[P]Open inline terminal" })
 vim.keymap.set('t', '<c-w>h', '<c-\\><c-n>', { silent = true })
-vim.keymap.set('t', '<esc>', '<c-\\><c-n>')
+vim.keymap.set('t', '<esc>', '<c-\\><c-n>', { desc = "[P]Return terminal into normal mode" })
