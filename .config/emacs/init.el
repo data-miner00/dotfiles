059c7a38a388f2a5d5f38368cb0cd156f2412c8f
Shaun Chong <pkay_@live.com>
2024-08-18 21:20:04 +0800

    Add basic emacs config

---
+++ .config/emacs/init.el
@@ -0,0 +1,7 @@
+(org-babel-load-file
+ (expand-file-name
+  "config.org"
+  user-emacs-directory))
+
+(setq inhibit-startup-message t         ; Don't show the splash screen
+      visible-bell t)                   ; Flash when the bell rings
